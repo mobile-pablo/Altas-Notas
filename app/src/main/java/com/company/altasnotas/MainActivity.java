a0754cdefb949d7d2510245e5ed416b0e9ca66c4
polonez-byte-112 <70174448+polonez-byte-112@users.noreply.github.com>
2021-04-11 20:41:26 +0200

    Bulding Bottom Navigation Bar

--- app/src/main/java/com/company/altasnotas/MainActivity.java
+++ app/src/main/java/com/company/altasnotas/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.main_nav_bottom);
+        bottomNavigationView.setItemIconTintList(null);
     }
 }
